e90277409cb12cf4f81c8cf73b1f2df86b0b5168
Nicolas Laverde <nicolas.laverde@quantcast.com>
2024-03-31 17:52:09 -0500

    nextJS projects

---
+++ 05-onwards-foodies-starting-project/app/meals/error.js
@@ -0,0 +1,7 @@
+'use client'
+export default function ErrorMeals() {
+    return <main className="error">
+        <h1> An error Ocurred</h1>
+        <p>Failed to fetch meal data. Please try again later</p>
+    </main>
+}
---
+++ 05-onwards-foodies-starting-project/app/meals/share/error.js
@@ -0,0 +1,7 @@
+'use client'
+export default function ErrorMeals() {
+    return <main className="error">
+        <h1> An error Ocurred</h1>
+        <p>Failed to create a meal</p>
+    </main>
+}
